205de87ff8c4287b4896d76ad86390c8937f508f
Kent Peifeng Ke <kepeifeng@gmail.com>
2017-04-16 12:13:31 +0800

    Initial Commit

---
+++ Circles/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Circles
+//
+//  Created by Kent Peifeng Ke on 2017/4/16.
+//  Copyright © 2017年 Kent Peifeng Ke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
